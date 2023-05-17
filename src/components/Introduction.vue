f24bec2eca698828ada5d5fb150f741c09b7b0d9
reyhanmichiels <michiels.reyhan@gmail.com>
2023-05-17 21:35:39 +0700

    fix bug

--- src/components/Introduction.vue
+++ src/components/Introduction.vue
@@ -1,29 +1,29 @@
 <script setup>
-import { reactive } from 'vue';
+import { reactive } from "vue";
 
 const card = reactive({
-    title: "Hello again!",
-    body: "My name is reyhan, i'm Technology Information student at Brawijaya University. I'm interested in becoming a backend developer.  ", 
-    button: "See portfolio"
-})
+  title: "Hello again!",
+  body: "My name is reyhan, i'm Technology Information student at Brawijaya University. I'm interested in becoming a backend developer.  Now I have experience with php and laravel, and am learning the GO language. In the future I'm interested in learning about springboot, docker, and a little bit about devops",
+  button: "See portfolio",
+});
 </script>
 
 <template>
-  <div id="introduction" class="w-screen h-screen flex justify-center items-center">
+  <div
+    id="introduction"
+    class="w-screen h-screen flex justify-center items-center"
+  >
     <div class="w-1/2 h-1/2">
       <div class="card lg:card-side bg-base-100 shadow-xl">
         <figure>
-          <img
-            src="../assets/profile.jpeg"
-            alt="Album"
-          />
+          <img src="../assets/profile.jpeg" alt="Album" />
         </figure>
         <div class="card-body">
           <h2 class="card-title">{{ card.title }}</h2>
           <p>{{ card.body }}</p>
           <div class="card-actions justify-end">
             <a href="#portfolio">
-            <button class="btn btn-primary">{{ card.button }}</button>
+              <button class="btn btn-primary">{{ card.button }}</button>
             </a>
           </div>
         </div>
@@ -31,4 +31,3 @@ const card = reactive({
     </div>
   </div>
 </template>
-
